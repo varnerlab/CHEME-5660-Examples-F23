879454d369a79e6edf7c10e957ecdeffdd860998
Jeffrey Varner <jdv27@cornell.edu>
2023-09-03 13:38:34 -0400

    T-bill price example completed
    completed

--- Include.jl
+++ Include.jl
@@ -3,6 +3,9 @@ const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 
+# make sure all is up to date -
+import Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update()
+
 # load external packages -
 using VLQuantitativeFinancePackage
 using DataFrames
@@ -16,5 +19,6 @@ using StatsPlots
 
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
-include(joinpath(_PATH_TO_SRC, "Computes.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Utility.jl"));
 
--- src/.ipynb_checkpoints/Files-checkpoint.jl
+++ src/.ipynb_checkpoints/Files-checkpoint.jl
@@ -5,5 +5,5 @@ end
 
 # Load harded coded specific files -
 function MyTreasuryBillDataSet()::DataFrame
-    return _loadcsvfile(joinpath(_PATH_TO_DATA, "US-TBill-Prices-TD-May-Sept-2023.csv"));
+    return _loadcsvfile(joinpath(_PATH_TO_DATA, "data/US-TBill-Prices-TD-May-Sept-2023.csv"));
 end
