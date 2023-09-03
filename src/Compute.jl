7aea157082a6489b9a948ce1e51473b141276bbe
Jeffrey Varner <jdv27@cornell.edu>
2023-09-03 15:48:11 -0400

    Bonds and Notes example
    bonds and notes example

--- src/Compute.jl
+++
@@ -1,3 +0,0 @@
-# Shortcut methods
-(compounding::DiscreteCompoundingModel)(model::MyUSTreasuryCouponSecurityModel) = _price_discrete_compounding(model::MyUSTreasuryCouponSecurityModel)
-(compounding::ContinuousCompoundingModel)(model::MyUSTreasuryCouponSecurityModel) = _price_continuous_compounding(model::MyUSTreasuryCouponSecurityModel)
