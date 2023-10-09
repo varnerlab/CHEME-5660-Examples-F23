dd96b95fba0d72a9eced832e7f466e87dbb22d35
Jeffrey Varner <jdv27@cornell.edu>
2023-10-09 06:49:20 -0400

    call and put
    call and put

--- src/Files.jl
+++ src/Files.jl
@@ -31,3 +31,4 @@ end
 
 MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "OHLC-Daily-SP500-5-years-TD-1256.jld2"));
 MyFirmMappingDataSet() = _loadcsvfile(joinpath(_PATH_TO_DATA, "SP500-Firm-Mapping-06-22-23.csv"));
+MyOptionsChainDataSet() = _loadcsvfile(joinpath(_PATH_TO_DATA, "AMD-options-exp-2023-08-18-monthly-07-18-2023.csv"));
