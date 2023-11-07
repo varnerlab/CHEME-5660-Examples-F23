168669ea9dcbd64bb47447d79cea927ac230034a
Jeffrey Varner <jdv27@cornell.edu>
2023-11-07 16:06:02 -0500

    working on example
    L12c example

--- src/Compute.jl
+++ src/Compute.jl
@@ -59,3 +59,30 @@ function log_growth_matrix(dataset::Dict{Int64,DataFrame},
     # return -
     return return_matrix;
 end
+
+function log_return_matrix(dataset::Dict{String, DataFrame}, 
+    firms::Array{String,1}; Δt::Float64 = (1.0/252.0), risk_free_rate::Float64 = 0.0)::Array{Float64,2}
+
+    # initialize -
+    number_of_firms = length(firms);
+    number_of_trading_days = nrow(dataset["AAPL"]);
+    return_matrix = Array{Float64,2}(undef, number_of_trading_days-1, number_of_firms);
+
+    # main loop -
+    for i ∈ eachindex(firms) 
+
+        # get the firm data -
+        firm_index = firms[i];
+        firm_data = dataset[firm_index];
+
+        # compute the log returns -
+        for j ∈ 2:number_of_trading_days
+            S₁ = firm_data[j-1, :volume_weighted_average_price];
+            S₂ = firm_data[j, :volume_weighted_average_price];
+            return_matrix[j-1, i] = (1/Δt)*log(S₂/S₁) - risk_free_rate;
+        end
+    end
+
+    # return -
+    return return_matrix;
+end
